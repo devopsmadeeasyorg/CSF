789e903c7f942555601503790bf91ab7d2d3b63a
krish <krishnamaram2@gmail.com>
2023-04-20 11:12:17 +0530

    add

--- provider-templates/azure/backend.tf
+++ provider-templates/azure/backend.tf
@@ -1,8 +1,8 @@
-terraform {
+/* terraform {
   backend "azurerm" {
     resource_group_name  = "Dev-RG"
     storage_account_name = "storageaccount2023q2"
     container_name       = "tfstatecontainer"
     key                  = "dev.terraform.tfstate"
   }
-}
+} */
