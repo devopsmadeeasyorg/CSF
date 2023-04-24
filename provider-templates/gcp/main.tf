0d59fd573c728857375ee2b889b9041d987faa57
krishnamaram2025 <112494492+krishnamaram2025@users.noreply.github.com>
2023-04-24 15:33:38 +0530

    Update main.tf

--- provider-templates/gcp/main.tf
+++ provider-templates/gcp/main.tf
@@ -1,9 +1,3 @@
-#resource "google_project" "my_project" {
-#  name       = "myroject"
-#  project_id = "myprojectid"
-#}
-
-
 resource "google_compute_network" "myvpc" {
   name                    = "myvpc"
   auto_create_subnetworks = "false"
