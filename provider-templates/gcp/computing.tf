53a6781d8af67eec605013eb231926cbbd7845e0
krish <krishnamaram2@gmail.com>
2023-06-01 22:49:34 +0530

    added

--- provider-templates/gcp/computing.tf
+++ provider-templates/gcp/computing.tf
@@ -1,3 +1,26 @@
+resource "google_compute_instance" "bastionhost" {
+  name         = "bastionhost"
+  machine_type = "e2-medium"
+  zone         = "us-west2-a"
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-7"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.mysubnet.id
+    access_config {
+      // Ephemeral public IP
+      nat_ip = google_compute_address.bastionpip.address
+    }
+}
+metadata = {
+  ssh-keys ="${var.mysshuser}:${var.mykey}"
+}
+}
+
 resource "google_compute_instance" "appservers" {
   count = 2
   name         = "appserver-${count.index + 1}"
@@ -17,37 +40,13 @@ resource "google_compute_instance" "appservers" {
 
   network_interface {
     subnetwork = google_compute_subnetwork.mysubnet.id
-    access_config {
-      // Ephemeral public IP
-      nat_ip = google_compute_address.static[count.index].address
-    }
+    # access_config {
+    #   // Ephemeral public IP
+    #   nat_ip = google_compute_address.static[count.index].address
+    # }
 }
 metadata = {
   ssh-keys ="${var.mysshuser}:${var.mykey}"
 }
-
- /*provisioner "remote-exec" {
-    connection {
-      host        = google_compute_address.static[count.index].address
-      type        = "ssh"
-      user        = "gcp-user"
-      timeout     = "500s"
-      private_key = file("~/.ssh/id_rsa")
-    }
-     inline = [
-      "sudo apt update",
-
-      "sudo apt install docker.io -y",
-
-      "sudo usermod -aG docker $USER && sudo chmod 777 /var/run/docker.sock",
-      
-      "sudo git clone https://github.com/csp2022/CSP.git && cd CSP/utils/flask",
-
-      "sudo docker image build -t flask .",
-
-      "sudo docker run -d --name flask -p 5001:5001 flask"
-        ] 
-        inline =["sudo yum install git -y"]
-  }*/
 }
 
--- provider-templates/gcp/networking.tf
+++ provider-templates/gcp/networking.tf
@@ -23,9 +23,43 @@ resource "google_compute_subnetwork" "myproxysubnet" {
 
 
 
-resource "google_compute_address" "static" {
-  count = 2
-  name = "staticip-${count.index + 1}"
+# resource "google_compute_address" "static" {
+#   count = 2
+#   name = "staticip-${count.index + 1}"
+#   region = "us-west2"
+#   depends_on = [ google_compute_firewall.myfw ]
+# }
+
+resource "google_compute_address" "bastionpip" {
+  name = "bastionpip"
   region = "us-west2"
   depends_on = [ google_compute_firewall.myfw ]
+}
+
+
+
+
+
+## Create Cloud Router
+
+resource "google_compute_router" "router" {
+  # project = var.project_name
+  name    = "nat-router"
+  network       = google_compute_network.myvpc.id
+  region  = "us-west2"
+}
+
+## Create Nat Gateway
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "my-router-nat"
+  router                             = google_compute_router.router.name
+  region        = "us-west2"
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
 }
