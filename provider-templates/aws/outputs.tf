8d2173119cc0a5d6197acb8d1d13531f903d8943
krish <krishnamaram2@gmail.com>
2023-06-09 23:33:12 +0530

    added

--- provider-templates/aws/outputs.tf
+++ provider-templates/aws/outputs.tf
@@ -3,7 +3,11 @@ value = "${aws_elb.elb_server.dns_name}"
 }
 
 output "bastionhost_public_ip" {
-  value = "ssh -A centos@${aws_instance.bastionhost.public_ip}"
+  value = "eval `ssh-agent` && ssh-add -k ~/.ssh/id_rsa && ssh -A centos@${aws_instance.bastionhost.public_ip}"
+}
+
+output "login_to_webapp-server-1" {
+  value = "ssh centos${aws_instance.webapp_server_1.private_ip}"
 }
 
 
