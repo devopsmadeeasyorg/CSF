63bd892e43793ca4143cb19d2c910ceab2e88740
krish <krishnamaram2@gmail.com>
2023-06-12 19:12:18 +0530

    added

--- provider-templates/azure/outputs.tf
+++ provider-templates/azure/outputs.tf
@@ -3,5 +3,11 @@ output "LB_public_ip" {
 }
 
 output "bastionhost_public_ip" {
-  value = azurerm_linux_virtual_machine.bastion.public_ip_address
+  value = "eval `ssh-agent` && ssh-add -k ~/.ssh/id_rsa && ssh -A azure-user@${azurerm_linux_virtual_machine.bastion.public_ip_address}"
 }
+
+# output "webapp_private_ip" {
+#   # value = azurerm_linux_virtual_machine.webapp.private_ip
+#   value = "ssh azure-user${azurerm_linux_virtual_machine.webapp.private_ip}"
+# }
+
